706f4b76467819e0be7362f4b0f779e5e0206d4f
m-gic <m-gic@outlook.com>
2026-04-14 19:31:22 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-// index.js
-const express = require('express'); // 必须引入 express
-const app = express();              // 初始化 app
+const http = require('http');
+const fs = require('fs');
+const exec = require("child_process").exec;
+const PORT = process.env.PORT || 3000; 
 
-const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/clever_cloud_intro.html');
+  // 使用 path.join 确保路径在服务器环境正确
+  res.sendFile(path.join(__dirname, 'clever_cloud_intro.html'));
 });
 
-// 注意：如果你直接使用 app.listen，就不需要额外定义 server 对象
 app.listen(port, () => {
   console.log(`应用已在端口 ${port} 上就绪`);
 });
