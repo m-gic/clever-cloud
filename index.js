b2792a6db183c834554966975e3175c0e5b4f54b
m-gic <m-gic@outlook.com>
2026-04-14 19:28:17 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // index.js
 const port = process.env.PORT || 8080; 
 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/clever_cloud_intro.html'); 
+});
+
 server.listen(port, () => {
   console.log(`应用已在端口 ${port} 上就绪`);
 });
